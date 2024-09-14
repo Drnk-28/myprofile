6612514b3774515735c6a7dd36408b2a463d67df
drnk.28 <fuadfuuk12345@gmail.com>
2024-09-15 04:29:38 +0700

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,65 @@
+export default defineNuxtConfig({
+   devtools: { enabled: true },
+   modules: [
+    "@nuxt/ui",
+    "nuxt-icon",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/fontaine",
+    "@nuxt/image",
+    "@nuxt/content",
+    "@nuxthq/studio",
+    "@vueuse/nuxt",
+    "@nuxtjs/i18n"
+  ],
+   ui: {
+      icons: ["heroicons", "lucide"],
+   },
+   app: {
+      pageTransition: { name: "page", mode: "out-in" },
+      head: {
+         htmlAttrs: {
+            lang: "en",
+            class: "h-full",
+         },
+         bodyAttrs: {
+            class: "antialiased bg-gray-50 dark:bg-black min-h-screen",
+         },
+      },
+   },
+   content: {
+      highlight: {
+         theme: "github-dark",
+      },
+   },
+   googleFonts: {
+      display: "swap",
+      families: {
+         Inter: [400, 500, 600, 700, 800, 900],
+      },
+   },
+   i18n: {
+      lazy: true,
+      langDir: "locales",
+      strategy: "no_prefix",
+      locales: [
+         {
+            name: "ID",
+            icon: "i-cirle-flags-id",
+            iso: "id",
+            code: "id",
+            file: "ID.json"
+        },
+         {
+            name: "EN",
+            icon: "i-cirle-flags-en",
+            iso: "en-US",
+            code: "en",
+            file: "EN.json"
+        }
+        ],
+      defaultLocale: "en",
+      vueI18n: {
+         fallbackLocale: "en",
+      },
+   }
+});
