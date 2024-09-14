5706badeb1e9e948a204998c2634182ca1f10007
drnk.28 <fuadfuuk12345@gmail.com>
2024-09-15 04:32:25 +0700

    Create Intro.vue

---
+++ components/Home/Intro.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="space-y-6">
+      <NuxtImg src="/me.png" alt="drnk.28" class="ring-2 border ring-primary-400 border-primary-300 dark:ring-white/10 dark:border-primary-200 hover:ring-4 transition-all duration-300 bg-primary-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16" sizes="100px sm:100px" placeholder format="webp" />
+      <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
+         Hi guys i'm
+      </h1>
+   </div>
+</template>
+
+<script setup>
+   useSeoMeta({
+      title: "drnk.28",
+      description: "I'm drnk and I specialize in building web applications and sites using Javascript, React, Vue & Node.",
+   });
+</script>
